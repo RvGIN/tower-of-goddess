48ef0c834707ff67f7ab459fd1ca939a498e0a90
RvGIN <138839237+RvGIN@users.noreply.github.com>
2025-01-05 10:00:57 +0800

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 function callback(el) {
   let labelGroups = body.querySelectorAll(`[name="${el.getAttribute('name')}"]`)
   Array.from(labelGroups).forEach(el => {
-    el.className = "text-4xl bg-transparent hover:bg-blue text-blue-dark font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded"
+    el.className = "md:text-4xl text-xl bg-transparent hover:bg-blue-500 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
   })
-  
-  el.className = "text-4xl bg-blue font-semibold text-white py-2 px-4 border border-transparent rounded"
+
+  el.className = "md:text-4xl text-xl bg-blue-500 font-semibold text-white py-2 px-4 border border-blue-500 rounded"
 }
 
 function calculate() {
@@ -25,18 +25,18 @@ function calculate() {
     "2121": "040",
     "2211": "400",
   }
-  
-  
+
+
   let emptyValue = body.querySelector('input[name="empty"]:checked')?.value
   let leftValue = body.querySelector('input[name="left"]:checked')?.value
   let centerValue = body.querySelector('input[name="center"]:checked')?.value
   let rightValue = body.querySelector('input[name="right"]:checked')?.value
-  
+
   let group = [emptyValue, leftValue, centerValue, rightValue]
-  if(group.filter(data => data === undefined).length > 0) return
-  
+  if (group.filter(data => data === undefined).length > 0) return
+
   let result = MAPPING[group.join('')]
-  if(result === undefined) {
+  if (result === undefined) {
     output.innerText = "此組合不存在，請重新確認。"
   } else {
     output.innerText = result
